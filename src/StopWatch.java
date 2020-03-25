bbb904e94dda24ea2a7bbc3be9c681b4a53891a9
linhhm24 <linh.hm24@gmail.com>
2020-03-25 08:11:32 +0700

    add file

---
+++ src/StopWatch.java
@@ -0,0 +1,31 @@
+
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public long getStartTime(){
+        return startTime;
+    }
+    public long getEndTime(){
+        return endTime;
+    }
+    public void setEndTime(long endTime){
+        this.endTime = endTime;
+    }
+    public void setStartTime(long startTime){
+        this.startTime = startTime;
+    }
+    public StopWatch(){
+        this.startTime = System.currentTimeMillis();
+    }
+    public void start(){
+        this.startTime = System.currentTimeMillis();
+    }
+    public void end(){
+        this.endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime(){
+        return this.endTime - this.startTime;
+    }
+}
