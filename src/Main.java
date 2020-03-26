1dedddb446d783c9d92fc9c0500962e5b740dbc3
linhhm2406 <linh.hm24@gmail.com>
2020-03-26 21:35:00 +0700

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,31 @@
+import jdk.nashorn.internal.runtime.JSONFunctions;
+
 public class Main {
     public static void swap(double num1, double num2) {
         double temp = num1;
         num1 = num2;
         num2 = temp;
     }
-    public static void selectionSort(double arr[]) {
-        int min;
-        for (int i = 0; i < arr.length - 1; i++) {
-            min = i;
-            for (int j = i + 1; j < arr.length; j++)
-                if (arr[j] < arr[min])
-                    min = j;
-
-            swap(arr[min], arr[i]);
-        }
-    }
-//    public static void selectionSort(double[] arr) {
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = 0; j < arr.length - 1; j++) {
-//                if (arr[j] < arr[j + 1]) {
-//                    swap(arr[j], arr[j + 1]);
-//                }
-//            }
+//    public static void selectionSort(double arr[]) {
+//        int min;
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            min = i;
+//            for (int j = i + 1; j < arr.length-1; j++)
+//                if (arr[j] < arr[min])
+//                    min = j;
+//
+//            swap(arr[min], arr[i]);
 //        }
 //    }
+    public static void selectionSort(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length -i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         StopWatch stopWatch = new StopWatch();
@@ -34,6 +36,7 @@ public class Main {
         stopWatch.start();
         selectionSort(arr);
         stopWatch.end();
+        System.out.println("Thoi gian th toan chay : ");
         System.out.println("Thoi gian thuat toan chay: " + stopWatch.getElapsedTime());
     }
 }
